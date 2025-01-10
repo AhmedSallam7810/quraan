3b5b51de48b5e3055217925487dc4d4540f33ae4
ahmedsallam <ahmedsallam7810@gmail.com>
2025-01-10 23:02:55 +0200

    add-new-migrations

--- database/migrations/2025_01_06_205245_create_quran_reading_user_table.php
+++ database/migrations/2025_01_06_205245_create_quran_reading_user_table.php
@@ -1,36 +1,36 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('quran_reading_user', function (Blueprint $table) {
-            $table->foreignIdFor(App\Models\QuranReading::class)
-                ->constrained()
-                ->restrictOnDelete()
-                ->restrictOnUpdate();
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      */
+//     public function up(): void
+//     {
+//         Schema::create('quran_reading_user', function (Blueprint $table) {
+//             $table->foreignIdFor(App\Models\QuranReading::class)
+//                 ->constrained()
+//                 ->restrictOnDelete()
+//                 ->restrictOnUpdate();
 
-            $table->foreignIdFor(App\Models\User::class)
-                ->constrained()
-                ->cascadeOnDelete()
-                ->cascadeOnUpdate();
+//             $table->foreignIdFor(App\Models\User::class)
+//                 ->constrained()
+//                 ->cascadeOnDelete()
+//                 ->cascadeOnUpdate();
 
-            $table->timestamps();
-        });
-    }
+//             $table->timestamps();
+//         });
+//     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('quran_reading_user');
-    }
-};
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function down(): void
+//     {
+//         Schema::dropIfExists('quran_reading_user');
+//     }
+// };
--- database/migrations/2025_01_06_205642_create_quran_permissions_table.php
+++ database/migrations/2025_01_06_205642_create_quran_permissions_table.php
@@ -1,33 +1,33 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('quran_permissions', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('front_image');
-            $table->string('back_image');
-            $table->foreignIdFor(App\Models\User::class)
-                ->constrained()
-                ->cascadeOnDelete()
-                ->cascadeOnUpdate();
-            $table->timestamps();
-        });
-    }
+// return new class extends Migration {
+//     /**
+//      * Run the migrations.
+//      */
+//     public function up(): void
+//     {
+//         Schema::create('quran_permissions', function (Blueprint $table) {
+//             $table->id();
+//             $table->string('name');
+//             $table->string('front_image');
+//             $table->string('back_image');
+//             $table->foreignIdFor(App\Models\User::class)
+//                 ->constrained()
+//                 ->cascadeOnDelete()
+//                 ->cascadeOnUpdate();
+//             $table->timestamps();
+//         });
+//     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('quran_permissions');
-    }
-};
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function down(): void
+//     {
+//         Schema::dropIfExists('quran_permissions');
+//     }
+// };
